10b2c1a28ddc1dc60fa825b644ae9ef48d0443a9
flashayy <89212162+flashayy@users.noreply.github.com>
2025-04-14 10:18:49 +0200

    ahojcau

--- pygame.py
+++ pygame.py
@@ -3,3 +3,20 @@ pygame.init()
 
 res = xres, yres = 500, 500
 screen = pygame.display.set_mode(res)
+
+hrame = True
+while hrame:
+    hodiny = pygame.time.Clock()
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            pygame.quit()
+            SystemExit()
+        if udalost.type == pygame.KEYDOWN:
+            if udalost.key == pygame.K_a:
+                x -= 10
+            if udalost.key == pygame.k_d:
+                x += 10
+    pygame.draw.rect(screen, (rr(0, 256), rr(256), rr(256)), (x, y, 200, 200))
+    pygame.display.update()
+    hodiny.tick(100)
+pygame.quit()
