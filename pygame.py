e97ea4a5e1e3973b9ecdee1344926950310281fb
flashayy <89212162+flashayy@users.noreply.github.com>
2025-04-14 11:21:42 +0200

    komat

--- pygame.py
+++ pygame.py
@@ -6,10 +6,43 @@ pygame.init()
 res = xres, yres = 500, 500
 screen = pygame.display.set_mode(res)
 
+class Player():
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+    
+    def draw(self, surface):
+        pygame.draw.rect(screen, (rr(0, 256), rr(256), rr(256)), (self.x, self.y, self.a, self.b))
+    
+    def move_right(self):
+        self.x += 1
+        
+    def move_left(self):
+        self.x -= 1
+        
+    def move_down(self):
+        self.y += 1
+        
+    def move_up(self):
+        self.y -= 1
+        
+    def inc(self):
+        self.a += 10
+        self.b += 10
+        
+    def dec(self):
+        self.a -= 10
+        self.b -= 10
+        
+player_one = Player(150, 150, 200, 200)
+        
 x, y = 150, 150
 a, b = 200, 200
 
 hrame = True
+
 while hrame:
     hodiny = pygame.time.Clock()
     for udalost in pygame.event.get():
@@ -18,21 +51,26 @@ while hrame:
             SystemExit()
         if udalost.type == pygame.KEYDOWN:
             if udalost.key == pygame.K_q:
-                a += 10
-                b += 10
+                player_one.inc()
             if udalost.key == pygame.K_e:
-                pygame.draw.rect(screen, (0, 0, 0), (x, y, a, b))
-                a -= 10
-                b -= 10
+                player_one.dec()
                 
-        klavesy = pygame.key.get_pressed()    
-        if klavesy [pygame.K_a]:
-                pygame.draw.rect(screen, (0, 0, 0), (x, y, a, b))
-                x -= 1
-        if klavesy [pygame.K_d]:
-                pygame.draw.rect(screen, (0, 0, 0), (x, y, a, b))
-                x += 1
-    pygame.draw.rect(screen, (rr(0, 256), rr(256), rr(256)), (x, y, a, b))
+    klavesy = pygame.key.get_pressed()
+
+    if klavesy[pygame.K_a]:
+        player_one.move_left()
+
+    if klavesy[pygame.K_d]:
+        player_one.move_right()
+
+    if klavesy[pygame.K_w]:
+        player_one.move_up()
+
+    if klavesy[pygame.K_s]:
+        player_one.move_down()
+    
+    screen.fill((0,0,0))
+    player_one.draw(screen)
     pygame.display.update()
-    hodiny.tick(100)
+    hodiny.tick(50)
 pygame.quit()
