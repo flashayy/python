1c3a3bed756da70243e46bf1a3c748fde2c22105
flashayy <89212162+flashayy@users.noreply.github.com>
2025-04-15 10:13:20 +0200

    ine tlacitak minecraft.py

--- work/minecraft/minecraft.py
+++ work/minecraft/minecraft.py
@@ -15,11 +15,11 @@ for i in range(20):
 def input(key):
     for box in boxes:
         if box.hovered:
-            if key == 'left mouse down':
+            if key == 'right mouse down':
                 new = Button(color=color.white, model='cube', position=box.position + mouse.normal,
                             texture = 'grass.png', parent=scene, origin_y=0.5)
                 boxes.append(new)
-            if key == 'right mouse down':
+            if key == 'left mouse down':
                 boxes.remove(box)
                 destroy(box)
 app.run()
